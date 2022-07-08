9fef4f73b9f4a6a624290dfb55e664bc44856dde
cyrozap <cyrozap@gmail.com>
2022-07-08 02:40:28 -0500

    Add command for testing memory

--- ASM236x/asm236x_tool.py
+++ ASM236x/asm236x_tool.py
@@ -129,6 +129,44 @@ def reload(args, dev):
 
     return 0
 
+def memtest(args, dev):
+    start_addr = int(args.address, 16)
+    test_len = args.length
+    stride = args.stride
+
+    print("Testing data from 0x{:04x} to 0x{:04x}, inclusive...".format(start_addr, start_addr+test_len-1))
+
+    zeros = bytes(test_len)
+
+    tests = (
+        ("Random 1", os.urandom(test_len)),
+        ("Zeros 1", zeros),
+        ("Random 2", os.urandom(test_len)),
+        ("Zeros 2", zeros),
+    )
+
+    for test_name, test_data in tests:
+        print("Running test \"{}\"...".format(test_name))
+
+        before = dev.read(start_addr, test_len, stride)
+        dev.write(start_addr, test_data)
+        after = dev.read(start_addr, test_len, stride)
+
+        if after != test_data:
+            print("Error: Failed test \"{}\"!".format(test_name))
+            for i in range(test_len):
+                if after[i] != test_data[i]:
+                    print(" + Mismatch at address 0x{:04x}: Expected 0x{:02x}, got 0x{:02x} instead.".format(
+                        start_addr + i, test_data[i], after[i]))
+                    break
+
+            if after == before:
+                print(" + Write had no effect--this is likely read-only memory.")
+
+            return 1
+
+    return 0
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--device", default="/dev/sg0", help="The SCSI/SG_IO device. Default: /dev/sg0")
@@ -155,6 +193,12 @@ def main():
 
     parser_reload = subparsers.add_parser("reload")
     parser_reload.set_defaults(func=reload)
+
+    parser_memtest = subparsers.add_parser("memtest")
+    parser_memtest.add_argument("-s", "--stride", type=int, default=1, help="The number of bytes to read with each SCSI command. Min: 1, Max: 255, Default: 1")
+    parser_memtest.add_argument("address", type=str, help="The address to start the test from, in hexadecimal.")
+    parser_memtest.add_argument("length", type=int, default=1, help="The total number of bytes to test. Default: 1")
+    parser_memtest.set_defaults(func=memtest)
 
     args = parser.parse_args()
 
