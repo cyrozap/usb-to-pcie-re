81821a6affdad74e1ea9196bd07bcffb5b66f3e6
cyrozap <cyrozap@gmail.com>
2024-03-12 01:15:27 -0500

    Add new JEYI ASM2464 firmware

--- ASM2x6x/firmware/download.sh
+++ ASM2x6x/firmware/download.sh
@@ -27,3 +27,4 @@ wget \
 curl -o downloads/JEYI2464_0525.zip https://web.archive.org/web/20231223011853if_/https://cdn.shoplazza.com/3d54d7ae5536d23a120f31065eed2b57.zip
 curl -o downloads/JEYI2464_0810_10min.zip https://web.archive.org/web/20231223011833if_/https://cdn.shoplazza.com/4788ee7e011b6f00f2c5f648c802f746.zip
 curl -o downloads/JEYI2464_1005_10Min.zip https://web.archive.org/web/20231223011831if_/https://cdn.shoplazza.com/e5816cde2955976ba163965bd067124d.zip
+curl -o downloads/ASM2464PD_FW_231218_85_00_00.zip https://web.archive.org/web/20240312061204if_/https://cdn.shoplazza.com/71af6d849f082e2d2e399a33110accb3.zip
